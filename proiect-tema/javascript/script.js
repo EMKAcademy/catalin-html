d175989ccaaf9859cd87b2692aebbc05f4282354
EMKAcademy <48829574+EMKAcademy@users.noreply.github.com>
2019-10-21 22:19:57 +0300

    tema + slider functional

---
+++ proiect-tema/javascript/script.js
@@ -0,0 +1,35 @@
+slideimg=document.getElementById('slide')
+s_index=1
+
+setInterval(slider,5000)
+
+function slider() {
+    slideimg.classList.remove('slide')
+    if (s_index==0) {
+        slide1()
+    } else {
+        slider2()
+    }
+}
+
+function slide1() {
+    
+    slideimg.classList.add('slide')
+        s_index=s_index+1;
+        slideimg.style.backgroundImage='url("/images/banners/banner03.jpg")';
+    slideimg.addEventListener('animationend', function()
+    {
+        slideimg.classList.remove('slide')
+    })
+}
+    
+
+function slider2(){
+    slideimg.classList.add('slide')
+        s_index=s_index-1;
+        slideimg.style.backgroundImage='url("/images/banners/banner02.jpg")';
+        slideimg.addEventListener('animationend', function()
+        {
+            slideimg.classList.remove('slide')
+        })
+}
